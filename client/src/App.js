28371a9966241f1fb3a7ba54cd8ee936feba12a2
Trung <109146090+trungtddevweb@users.noreply.github.com>
2022-12-16 23:36:12 +0700

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
-  return (
-    <div>
-      <header>
-        <a href="/">MyShopping</a>
-      </header>
-      <main>List</main>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <header>
+                    <Link to="/">MyShopping</Link>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<HomeScreen />} />
+                        <Route
+                            path="/product/:slug"
+                            element={<ProductScreen />}
+                        />
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
